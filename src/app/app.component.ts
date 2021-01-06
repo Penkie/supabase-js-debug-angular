68c2ec4dc417267dab9b43c4de7c6a2cef2ca8ba
Penkie <oscar@oscarprince.ch>
2021-01-06 19:56:37 +0100

    added supabase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    const supabaseUrl = "https://cqyttyghuetwtynvqfgf.supabase.co";
+    const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYwOTc3MTA0MSwiZXhwIjoxOTI1MzQ3MDQxfQ.hDn6RlaFMXitADJ-NCgB51JKzGWrR0rQ3GPzZv2hVmw";
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    console.log(supabase);
+  }
 }
